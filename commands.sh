a257428e6c5f090b0eed79d53e94fe7c78358606
Sasi Kumar T <sasikt@thoughtworks.com>
2020-07-08 20:03:22 +0530

    Initial project setup for elixir

---
+++ commands.sh
@@ -0,0 +1,10 @@
+mix new cards
+
+apm install atom-elixir-formatter
+apm install language-elixir
+apm install atom-elixir
+
+
+
+iex -S mix
+recompile
---
+++ lib/cards.ex
@@ -0,0 +1,22 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for `Cards`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cards.hello()
+      :world
+
+  """
+  def create_deck do
+    ["Ace", "Two", "Three"]
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+end
