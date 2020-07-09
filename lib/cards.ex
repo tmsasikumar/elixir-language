241eec12989203a9d7e4d973a6faf0207f8d8070
Sasi Kumar T <sasikt@thoughtworks.com>
2020-07-09 12:44:16 +0530

    Added double iteration for slot and values

--- lib/cards.ex
+++ lib/cards.ex
@@ -13,10 +13,21 @@ defmodule Cards do
 
   """
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for value <- values do
+      for suit <- suits do
+        "#{value} of #{suit}"
+      end
+    end
   end
 
   def shuffle(deck) do
     Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
   end
 end
