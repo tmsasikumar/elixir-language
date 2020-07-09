6e056cc2242bf65876657de2c2a247a66771ed84
Sasi Kumar T <sasikt@thoughtworks.com>
2020-07-09 14:49:45 +0530

    Added file loaders

--- lib/cards.ex
+++ lib/cards.ex
@@ -31,4 +31,22 @@ defmodule Cards do
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    {status, binary} = File.read(filename)
+
+    case status do
+      :ok -> :erlang.binary_to_term(binary)
+      :error -> "File does not exist in system"
+    end
+  end
 end
