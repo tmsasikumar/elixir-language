18443ba85e7f235db2d9c18468ff0d2786fb6f9d
Sasi Kumar T <sasikt@thoughtworks.com>
2020-07-09 12:45:30 +0530

    Add flatten to flatten as single list

--- lib/cards.ex
+++ lib/cards.ex
@@ -16,11 +16,14 @@ defmodule Cards do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-    for value <- values do
-      for suit <- suits do
-        "#{value} of #{suit}"
+    cards =
+      for value <- values do
+        for suit <- suits do
+          "#{value} of #{suit}"
+        end
       end
-    end
+
+    List.flatten(cards)
   end
 
   def shuffle(deck) do
